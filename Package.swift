12bec67b6d7c0f8bc5b2df14c07216f0e6279d45
Tom Everton <tom.everton@permutive.com>
2026-07-01 16:34:31 +0100

    Release v2.6.1

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name:"Permutive_tvOS",
-		url:"https://storage.googleapis.com/permutive-ios-sdks/swift-sdk/Permutive_tvOS-v2.6.0.zip",
-		checksum:"8f3bb2733790cab00782415330df301fcc723655adf3791b6b5726a834a104fd")
+		url:"https://storage.googleapis.com/permutive-ios-sdks/swift-sdk/Permutive_tvOS-v2.6.1.zip",
+		checksum:"0abe4174c9e31f48964daa859d4c202e364055a6ae386cebba1bd120041e7887")
     ]
 )
